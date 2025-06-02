c7bae12d28aebfac1ee9b6de4c1d18fff10ed555
Manuel Gómez <mnlgmontero@gmail.com>
2025-06-02 18:37:26 +0000

    Change to hi

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ app = FastAPI(title="Fast Api Hello World", version="1.0.0")
 @app.get("/hello", response_model=Greeting, tags=["greetings"])
 async def hello(name)->Greeting:
     """Hello."""
-    return {"msg": f'Hello {name}'}
+    return {"msg": f'Hi {name}'}
