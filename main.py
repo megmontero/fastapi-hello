5457f2f9b2d0f130132cd003c0df4cfc0f66d0fd
Manuel Gómez <mnlgmontero@gmail.com>
2025-06-02 07:24:10 +0000

    Agregamos version.

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
+
+
+# 1. Leer la versión desde el archivo
+try:
+    with open("version.txt", "r", encoding="utf-8") as f:
+        VERSION = f.read().strip()
+except FileNotFoundError:
+    VERSION = "1.0.0"  # Versión por defecto si el archivo no existe
+
+
+
 class Greeting(BaseModel):
     msg: str
 
 
-app = FastAPI(title="Fast Api Hello World", version="1.0.0")
+app = FastAPI(title="Fast Api Hello World", version=VERSION)
 
 
 @app.get("/hello", response_model=Greeting, tags=["greetings"])
 async def hello(name)->Greeting:
     """Hello."""
-    return {"msg": f'Hello {name}'}
+    return {"msg": f'Hi {name}! We are using jenkins from v{VERSION}.'}
